165044da96f1c5cb830256c049543412a60dea10
Ayaz Alam <ayumallick7079@gmail.com>
2024-09-12 20:19:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function openNavbar() {
+    document.getElementById("sideNavigationBar").style.width = "250px";
+}
+
+function closeNavbar() {
+    document.getElementById("sideNavigationBar").style.width = "0";
+}
